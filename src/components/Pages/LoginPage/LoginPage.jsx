1070f8e43988362c437da48382d11ce33b0fc766
Marek Rewers <mrewers@users.noreply.github.com>
2019-07-04 01:08:00 -0400

    Add log in page component

---
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './LoginPage.css';
+
+const LoginPage = () => (
+  <article className='login-container'>
+    <div className='login-modal'>
+      <h4>Please Login to Continue</h4>
+      <button className='login-button'>Login</button>
+    </div>
+  </article>
+);
+
+export default LoginPage;
