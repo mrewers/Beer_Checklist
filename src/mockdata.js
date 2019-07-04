f66edf90ceb70dfc598d692b526f0e10c4deec0a
Marek Rewers <mrewers@users.noreply.github.com>
2019-07-03 23:35:28 -0400

    Move mock data into separate file

---
+++ src/mockdata.js
@@ -0,0 +1,7 @@
+export const allBeers = [
+  { id: 1, name: 'beer1', brewery: 'brewery1', checked: true, group: 'group1' },
+  { id: 2, name: 'beer2', brewery: 'brewery1', checked: false, group: 'group2' },
+  { id: 3, name: 'beer3', brewery: 'brewery1', checked: false, group: 'group2' },
+];
+
+export const groups = ['group1', 'group2'];
