5c61c16ca2aff6185d3b4cb6a8252f31fc6136f4
Marek Rewers <mrewers@users.noreply.github.com>
2019-07-03 23:40:46 -0400

    Add context to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,39 @@
 import React, { useState } from 'react';
 
-import List from './List';
+import List from './List/List';
 import GroupedList from './GroupedList';
+import Options from './Options/Options';
 
 import './App.css';
 
-const beers = [
-  { id: 1, name: 'beer1', brewery: 'brewery1', checked: true, group: 'group1' },
-  { id: 2, name: 'beer2', brewery: 'brewery1', checked: false, group: 'group2' },
-  { id: 3, name: 'beer3', brewery: 'brewery1', checked: false, group: 'group2' },
-];
+import { unfinishedBeers } from '../apis/db';
+import { allBeers, groups } from '../mockdata';
 
-const groups = ['group1', 'group2'];
+export const AppContext = React.createContext();
 
 const App = () => {
-  const [grouped, setGrouped] = useState(true);
+  const [showFinished, setShowFinished] = useState(true);
+  const [showGroups, setShowGroups] = useState(true);
+
+  const toggleFinished = () => {
+    setShowFinished(!showFinished);
+  };
+
+  const toggleGroups = () => {
+    setShowGroups(!showGroups);
+  };
+
+  const beers = showFinished ? allBeers : unfinishedBeers;
+
   return (
     <section>
       <h1 className='app-header'>RIP IIP</h1>
-      {grouped && <GroupedList beers={beers} groups={groups} />}
-      {!grouped && <List beers={beers} />}
+      <AppContext.Provider value={{ toggleFinished, toggleGroups }}>
+        <Options />
+        <hr />
+        {showGroups && <GroupedList beers={beers} groups={groups} />}
+        {!showGroups && <List beers={beers} />}
+      </AppContext.Provider>
     </section>
   );
 };
