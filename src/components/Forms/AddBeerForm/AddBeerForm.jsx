fcbc185663e3c16a8657ceeaae8560b93f20b117
Marek Rewers <mrewers@users.noreply.github.com>
2019-07-05 22:14:31 -0400

    Pull in groups from firebase

--- src/components/Forms/AddBeerForm/AddBeerForm.jsx
+++ src/components/Forms/AddBeerForm/AddBeerForm.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { object } from 'prop-types';
 
 import Form from '../Form/Form';
-import { groups } from '../../../mockdata';
+import { withFirebase } from '../../../firebase';
 
 import './AddBeerForm.css';
 
@@ -10,10 +11,30 @@ class AddBeerForm extends Component {
     beer: '',
     brewery: '',
     group: '',
+    groups: [],
   };
 
+  componentDidMount() {
+    const { firebaseGetGroups } = this.props.firebase;
+    const groups = [];
+
+    firebaseGetGroups()
+      .get()
+      .then(querySnapshot =>
+        querySnapshot.forEach(doc => {
+          const group = {
+            id: doc.id,
+            title: doc.data().title,
+          };
+          groups.push(group);
+        })
+      );
+
+    this.setState({ groups });
+  }
+
   render() {
-    const { beer, brewery, group } = this.state;
+    const { beer, brewery, group, groups } = this.state;
 
     const handleChange = e => {
       const { name, value } = e.target;
@@ -30,26 +51,37 @@ class AddBeerForm extends Component {
     return (
       <Form legend='Add A Beer' submit='Add Beer' onSubmit={handleSubmit}>
         <div className='add-beer-form-fields'>
-          <label className='add-beer-form-label' htmlFor='name'>
+          <label className='add-beer-form-label' htmlFor='beer'>
             Beer Name:
             <input id='beer' name='beer' onChange={handleChange} type='text' value={beer} />
           </label>
-          <label className='add-beer-form-label' htmlFor='Brewery'>
+          <label className='add-beer-form-label' htmlFor='brewery'>
             Brewery:
             <input id='brewery' name='brewery' onChange={handleChange} type='text' value={brewery} />
           </label>
-          <select id='group' name='group' onChange={handleChange} onBlur={handleChange} value={group}>
-            <option value=''>Please select a group</option>
-            {groups.map(group => (
-              <option key={group} value={group}>
-                {group}
-              </option>
-            ))}
-          </select>
+          <label className='add-beer-form-label' htmlFor='price'>
+            Beer Price:
+            <input id='price' name='price' onChange={handleChange} type='text' value={brewery} />
+          </label>
+          <label className='add-beer-form-label' htmlFor='group'>
+            Add to Group:
+            <select id='group' name='group' onChange={handleChange} onBlur={handleChange} value={group}>
+              <option value=''>Please select a group</option>
+              {groups.map(group => (
+                <option key={group.id} value={group.title}>
+                  {group.title}
+                </option>
+              ))}
+            </select>
+          </label>
         </div>
       </Form>
     );
   }
 }
 
-export default AddBeerForm;
+AddBeerForm.propTypes = {
+  firebase: object,
+};
+
+export default withFirebase(AddBeerForm);
