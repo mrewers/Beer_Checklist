12c171392ce941c51017b9459b16361ab896594b
Marek Rewers <mrewers@users.noreply.github.com>
2019-07-06 00:14:27 -0400

    Delete out mockdata

--- src/mockdata.js
+++
@@ -1,7 +0,0 @@
-export const allBeers = [
-  { id: 1, name: 'beer1', brewery: 'brewery1', checked: true, group: 'group1' },
-  { id: 2, name: 'beer2', brewery: 'brewery1', checked: false, group: 'group2' },
-  { id: 3, name: 'beer3', brewery: 'brewery1', checked: false, group: 'group2' },
-];
-
-export const groups = ['group1', 'group2'];
